edf4396d0a8faa9a00750adff630fbbba3bcba8d
Eugene Otieno <eugenegregs@gmail.com>
2019-07-07 15:18:47 +0300

    Tasks select limit included

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,17 +3,27 @@ const router = express.Router();
 const Task = require('../src/models/Task');
 const sequelize = require('../src/database/connection');
 
+let Gpage;
+let perPage;
+
 router.get('/assigned', (req, res) => {
     const { page, limit, order } = req.query;
-    const offset = page * limit;
-    const recLimit = offset + limit;
-    Task.findAll({
-        recLimit,
+    Gpage = +page;
+    perPage = +limit;
+    const offset = ( +page - 1 ) * +limit;
+    Task.findAndCountAll({
         offset,
+        limit: +limit,
         order: sequelize.col(order)
     })
-    .then( tasks =>{
-        res.send(tasks)
+    .then( tasks => {
+        const response = {
+            totalTasks: tasks.count,
+            page: Gpage,
+            perPage: perPage,
+            tasks: tasks.rows
+        }
+        res.send(response);
     })
 })
 
